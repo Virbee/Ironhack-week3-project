c2a792f2ed208bd85650b5ba535a73f2a6879617
Virginie Beernaert <vbeernaert@yahoo.fr>
2019-06-08 18:30:30 +0200

    static html with 6 characters

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var characters = [
+  { name: Stefan, img: images(1).jpeg },
+  { name: Pablo, img: images.jpeg },
+  { name: Laura, img: download.jpeg },
+  { name: Karl, img: download(1).jpeg }
+];
