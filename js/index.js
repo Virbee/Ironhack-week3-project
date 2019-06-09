5ec1290fc6e1e079dfda77a8985a49e2945060e7
Virginie Beernaert <vbeernaert@yahoo.fr>
2019-06-09 14:32:26 +0200

    add a dynamic dysplaying

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 var characters = [
-  { name: Stefan, img: images(1).jpeg },
-  { name: Pablo, img: images.jpeg },
-  { name: Laura, img: download.jpeg },
-  { name: Karl, img: download(1).jpeg }
+  { name: "Stefan", img: "../images/001-boy.svg" },
+  { name: "Pablo", img: "../images/003-nerd.svg" },
+  { name: "Laura", img: "../images/021-girl.svg" },
+  { name: "Karl", img: "../images/028-engineer-1.svg" },
+  { name: "Ifemelu", img: "../images/030-policewoman.svg" },
+  { name: "Naïma", img: "../images/026-old-woman.svg" }
 ];
+
+//the document is loaded
+document.addEventListener("DOMContentLoaded", function(event) {
+  var characterDisplayed = "";
+  characters.forEach(function(character) {
+    characterDisplayed += '<p class="character">';
+    characterDisplayed += `<img src="` + character.img + `">`;
+    characterDisplayed += `<span>` + character.name + `</span>`;
+    characterDisplayed += `</p>`;
+  });
+  document.querySelector("#my_board").innerHTML = characterDisplayed;
+});
